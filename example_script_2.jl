7cebe29ab5026094a2400f2544b3811245d03e67
ILikeTheCode <79585703+ILikeTheCode@users.noreply.github.com>
2021-09-23 23:46:59 +1000

    Restore task 1 code

--- example_script_2.jl
+++ example_script_2.jl
@@ -11,37 +11,61 @@ end
 
 x = x_poly()
 
-p1 = x^2 + x^5
-p2 = x^5 + x^3
+p1 = 11x^2 + 52x^5
+p2 = 103x^5 + 7x^3
+prime = 5
 
-# println("To showcase some of the functions within this repo, we will use the two polynomials p1 = ", pretty_printer(p1), " and p2 = ", pretty_printer(p2), "\n")
+println("To showcase some of the functions within this repo, we will use the two polynomials p1 = ", pretty_printer(p1), " and p2 = ", pretty_printer(p2), "\n")
 
-# println("The scripts in this repo allow you to:\n")
-# println("Add polynomials together:")
-# println("p1 + p2 = ", pretty_printer(p1+p2), "\n")
+println("The scripts in this repo allow you to:\n")
+println("Add polynomials together:")
+println("p1 + p2 = ", pretty_printer(p1+p2), "\n")
 
-# println("Multiply polynomials and raise them by a power:")
-# println("p1 * p2 = ", pretty_printer(p1*p2))
-# println("p1^3 = ", pretty_printer(p1^3), "\n")
+println("Multiply polynomials and raise them by a power:")
+println("p1 * p2 = ", pretty_printer(p1*p2))
+println("p1^3 = ", pretty_printer(p1^prime), "\n")
 
-# println("Divide one polynomial by another:")
-# println("p1/p2 = ", pretty_printer(divide(p2, p1)(4)[1]), "\n")
+println("Divide one polynomial by another:")
+println("p1/p2 = ", pretty_printer((p2÷p1)(prime)), "\n")
 
-# println("Take the derivative of a polynomial:")
-# println("p1' * p2 = ", pretty_printer(derivative(p1*p2)))
-# println("p1' * p2 + p1 * p2' = ", derivative(p1)*p2 + p1*derivative(p2), "\n")
+println("Find the remainder of this polynomial division")
+println(rem(p1,p2)(prime), "\n")
 
-# println("Find the GCD of two polynomials modulo prime:")
-# println("GCD(p1,p2)(mod 3) = ", pretty_printer(gcd(p1,p2,3)), "\n")
+println("Take the derivative of a polynomial:")
+println("p1' * p2 = ", pretty_printer(derivative(p1*p2)))
+println("p1' * p2 + p1 * p2' = ", derivative(p1)*p2 + p1*derivative(p2), "\n")
 
-# println("Factorize polynomials:")
-# prime = 5
-# p = mod(p1,prime)
-# println("Will factor p1(mod $prime): ", p)
-# factorization = factor(p,prime)
-# println("Here is the factorization: ", factorization)
+println("Find the GCD of two polynomials modulo prime:")
+println("GCD(p1,p2)(mod 3) = ", pretty_printer(gcd(p1,p2,prime)), "\n")
 
-# pr = mod(expand_factorization(factorization),prime)
-# println("Reconstructing: ", pr)
+println("Factorize polynomials:")
+p = mod(p1,prime)
+println("Will factor p1(mod $prime): ", p)
+factorization = factor(p,prime)
+println("Here is the factorization: ", factorization)
 
-# println("Aside from the functions showcased, this repo does much much more!")
+pr = mod(expand_factorization(factorization),prime)
+println("Reconstructing: ", pretty_printer(pr), "\n")
+
+println("Make a polynomial square free:")
+println("The square free version of the polynomial, ", pretty_printer(p1), " is, ", pretty_printer(square_free(p1, prime)), "\n")
+
+println("Construct a cyclotonic polynomial given a prime integer input:")
+println("The following is a cyclotonic polynomial, ", pretty_printer(cyclotonic_polynomial(prime)), "\n")
+
+println("Construct a linear monic polynomial given an integer input:")
+println("The following is a linear monic polynomial generated with input $prime, ", pretty_printer(linear_monic_polynomial(prime)), "\n")
+
+println("Make a polynomial primitive:")
+println("The primitive part of the polynomial ", pretty_printer(p1), " is ", pretty_printer(prim_part(p1)(prime)), "\n")
+
+println("Take the modulus of a polynomial:")
+println(pretty_printer(p1), " modulo $prime is congruent to ", pretty_printer(mod(p1, prime)), "\n")
+
+println("Find the degree of a polynomial:")
+println("The degree of the polynomial ", pretty_printer(p1), " is equal to ", degree(p1), "\n")
+
+println("Find the leading term of any given polynomial:")
+println("The leading term of the polynomial ", pretty_printer(p1), " is equal to ", pretty_printer(Polynomial(leading(p1))), "\n")
+
+println("Aside from the functions showcased, this software package has much more functionality! \nIt can also perform arithmetic on individual polynomial terms, manipulate polynomials \nprogrammatically with pop! and push! and much, much more!")
